fb2807c314134a2bab4a5933c1f1963a015636f8
tjrelyts <tylerj-santo@outlook.com>
2024-04-11 19:44:10 -0400

    Recreated Naive Bayes model. Implemented ability to save model and vectorizer in order to load in app.py and run predictions directly. Currently at 85% accuracy.

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template, request
-from model import analyze
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
 
 app = Flask(__name__)
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+def analyze(str):
+    vec = vectorizer.transform(str)
+    y_pred = model.predict(vec)
+
+    if y_pred[0] == 2:
+        print("Sentiment: Negative")
+    elif y_pred[0] == 1:
+        print("Sentiment: Positive")
+    else:
+        print("Sentiment: Neutral")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         msg = request.form.get('msg')
-        print(analyze(msg))
-    return render_template("index.html")
+        analyze([msg])
+    return render_template("base.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
